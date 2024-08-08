416f0222ef24edd6955cc0a810667cb0e75a7b2f
8orannaro8 <oran03@naver.com>
2024-08-08 12:28:11 +0900

    Add files via upload

--- 소스코드/alu.v
+++ 소스코드/alu.v
@@ -6,8 +6,7 @@ module alu(
     input [31:0] aluin1,     // pc는 aluin1으로 받겠음
     input [31:0] aluin2,     // imm, shamt는 aluin2으로 받겠음
     output reg [31:0] aluout,
-    output reg [31:0] slt,
-    output reg [31:0] sltu
+    output reg [31:0] slt   
     );
 
     // add sub lui auipc part
@@ -32,7 +31,7 @@ module alu(
     assign overflow = (aluin1[31] != aluin2[31]) && (sum[31] != aluin1[31]);
     assign slt_alu = {31'b0, (sum[31]^overflow)};             
     assign sltu_alu = ((aluin1 < aluin2) ? 32'b1 :32'b0); // unsigned를 위한 단순 비교
-   
+    
     // sll part
     wire [31:0] sll;
     assign sll = aluin1 << aluin2;
@@ -66,42 +65,34 @@ module alu(
             4'h0: begin 
                   aluout <= sum;
                   slt <= 0;
-                  sltu <= 0; 
                   end
             4'h1: begin 
                   aluout <= sum;
-                  slt <= slt_alu;
-                  sltu <= sltu_alu; 
+                  slt <= slt_alu | sltu_alu;
                   end
             4'h2: begin
                   aluout <= sll;
                   slt <= 0;
-                  sltu <= 0; 
                   end
             4'h3: begin
                   aluout <= xor_alu;
                   slt <= 0;
-                  sltu <= 0; 
                   end
             4'h4: begin
                   aluout <= srl;
                   slt <= 0;
-                  sltu <= 0; 
                   end
             4'h5: begin
                   aluout <= sra;
                   slt <= 0;
-                  sltu <= 0; 
                   end
             4'h6: begin
                   aluout <= or_alu;
                   slt <= 0;
-                  sltu <= 0; 
                   end
             4'h7: begin
                   aluout <= and_alu;
                   slt <= 0;
-                  sltu <= 0; 
                   end
             //4'h8:
     
