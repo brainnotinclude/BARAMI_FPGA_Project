32a55739d3102f85900cbd320f047a223af7f412
Jeyun Park <jeyunp@hanyang.ac.kr>
2024-08-09 00:32:23 +0900

    Modify:multipliers

---
+++ 소스코드/multiplier.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/08/08 10:12:42
+// Design Name: 
+// Module Name: mutiplier
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module multiplier(
+    input [31:0] a,
+    input [31:0] b,
+    output reg [63:0] out
+    );
+    
+    integer i;
+    
+    reg [63:0] temp [31:0];
+    always@(*)begin
+        out = 0;
+        for(i = 0; i<32; i=i+1) begin
+            if(b[i] == 1'b1) begin
+                temp[i] = a << i;
+            end
+            else begin
+                temp[i] = 0;
+            end
+        end
+ 
+        for(i = 0; i<32; i=i+1) begin
+            out = out + temp[i];
+        end
+    end
+endmodule
--- 소스코드/multiplier_all.v
+++ 소스코드/multiplier_all.v
@@ -30,19 +30,19 @@ module multiplier_all(
     
     reg [31:0] a_temp;
     reg [31:0] b_temp;
-    reg [63:0] out_temp;
+    wire [63:0] out_temp;
     
     //mode 00: unsigned*unsigned, mode 01: signed*unsigned, mode 10:signed*signed
     
     multiplier mult(
         .a(a_temp),
         .b(b_temp),
-        .out(out-temp)
+        .out(out_temp)
     );
     
     //Convert negative values into corresponding positive values(2's complement)
-    always@(a) begin
-        if(mode != 2'b00 && a[31] == 1'b1) begin            //if a=negative
+    always@(*) begin
+        if(mode != 2'b00 && a[31] == 1'b1) begin            //if a=negative and not unit*unit
             a_temp = ~a+1'b1;
         end
         else begin
@@ -50,8 +50,8 @@ module multiplier_all(
         end 
     end
     
-    always@(b) begin
-        if(mode == 2'b10 && b[31] == 1'b1) begin            //if b=negative
+    always@(*) begin
+        if(mode == 2'b10 && b[31] == 1'b1) begin            //if b=negative and signed*signed
            b_temp = ~b+1'b1;
         end
         else begin
